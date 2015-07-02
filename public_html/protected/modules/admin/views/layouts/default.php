4c6bf15b5ca292d7b76112997a9907b7e32c0d34
Andrey Zhilin <a.zhilin@webinar.ru>
2015-07-02 20:34:09 +0300

    Inverse admin navbar

--- public_html/protected/modules/admin/views/layouts/default.php
+++ public_html/protected/modules/admin/views/layouts/default.php
@@ -2,7 +2,7 @@
 /* @var $this DefaultUserController */
 ?>
 <?php $this->beginContent('//layouts/main'); ?>
-    <nav class="navbar navbar-default">
+    <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
