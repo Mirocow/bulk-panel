0ce10e8fadddd1f7f89ea8ea790bc2837b873adf
a.zhilin <a.zhilin@webinar.ru>
2015-07-22 18:24:52 +0300

    VK

--- public_html/test/index.php
+++ public_html/test/index.php
@@ -1,12 +1,14 @@
 <head>
-    <script type="text/javascript" src="//vk.com/js/api/openapi.js"></script>
+    <script type="text/javascript" src="//vk.com/js/api/openapi.js?116"></script>
 </head>
 <body>
-<script>
-    VK.init({apiId: 5005271});
-</script>
-<div id="vk_auth"></div>
-<script type="text/javascript">
-    VK.Widgets.Auth("vk_auth", {width: "300px", authUrl: '/vklogin.php?'});
-</script>
+    <script>
+        VK.init({apiId: 5005271});
+    </script>
+    <div id="vk_auth"></div>
+    <script type="text/javascript">
+        VK.Widgets.Auth("vk_auth", {width: "200px", onAuth: function(data) {
+            console.log(data);
+        } });
+    </script>
 </body>
