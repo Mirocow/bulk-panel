4460b730f04520bb1fc2dba5a24b3fb11584e2f5
a.zhilin <a.zhilin@webinar.ru>
2015-07-22 18:13:29 +0300

    VK

--- public_html/test/index.php
+++ public_html/test/index.php
@@ -1,1 +1,12 @@
-1
+<head>
+    <script type="text/javascript" src="http://userapi.com/js/api/openapi.js?34"></script>
+</head>
+<body>
+<script>
+    VK.init({apiId: 4716867});
+</script>
+<div id="vk_auth"></div>
+<script type="text/javascript">
+    VK.Widgets.Auth("vk_auth", {width: "300px", authUrl: '/vklogin.php?'});
+</script>
+</body>
