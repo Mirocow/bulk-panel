e2647049df80a93a4bd8fbddf76e8917e183e7c9
a.zhilin <a.zhilin@webinar.ru>
2015-07-22 18:52:26 +0300

    VK

--- public_html/test/index.php
+++ public_html/test/index.php
@@ -5,10 +5,18 @@
     <script>
         VK.init({apiId: 5005271});
     </script>
-    <div id="vk_auth"></div>
+    <div id="lol" onclick="login()">1</div>
     <script type="text/javascript">
-        VK.Widgets.Auth("vk_auth", {width: "200px", onAuth: function(data) {
-            console.log(data);
-        } });
+        function login(){
+        VK.Auth.login(function(response) {
+            console.log(response);
+            if (response.session) {
+                if (response.settings) {
+                    /* Выбранные настройки доступа пользователя, если они были запрошены */
+                }
+            } else {
+                /* Пользователь нажал кнопку Отмена в окне авторизации */
+            }
+        });}
     </script>
 </body>
